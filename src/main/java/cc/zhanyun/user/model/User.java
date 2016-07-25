e66140c8cc5d42f72cee72a6a1a12edcc70ecaac
Administrator <Administrator@192.168.23.2>
2016-07-25 22:20:30 +0800

    首次提交

---
+++ src/main/java/cc/zhanyun/user/model/User.java
@@ -0,0 +1,12 @@
+package cc.zhanyun.user.model;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+	@Id
+	private String oid;
+	private String username;
+	private String password;
+	private String username;
+	
+}
